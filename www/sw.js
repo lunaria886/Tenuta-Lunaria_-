0d07fd9b266842498d03fdca8167ab8f055cee4e
lunaria886 <morenaspinelli822@gmail.com>
2026-08-12 23:59:00 +0200

    Revise service worker for cache versioning and management
    Updated service worker to version 2 and added cache management during activation.

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,26 @@
-const CACHE='tenuta-lunaria-v1';
+const CACHE='tenuta-lunaria-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, resClone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
